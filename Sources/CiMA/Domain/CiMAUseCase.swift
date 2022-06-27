b638fcd2395e74a4a46e8a9e948ae26af943698d
Alvaro Grimal Cabello <alvarogcab@ext.inditex.com>
2022-06-27 15:01:03 +0200

    CiMAUseCase compatibility with Combine and async/await

--- Sources/CiMA/Domain/CiMAUseCase.swift
+++ Sources/CiMA/Domain/CiMAUseCase.swift
@@ -6,127 +6,88 @@
 //
 
 import Foundation
+import Combine
 
-open class CiMAUseCase<Input, Output, Process> {
+open class CiMAUseCase<Input, Output> {
     
     // MARK: - Properties
-    private var onCompletionStopped: (() -> Void)?
     
     /**
-     Thread where the `handle(_:_:_:_:)` function will be executed.
+     Thread where the `handle(_:) -> CiMAObservable<Output>` function will receive elements from the publisher.
+     This variable can be overridden to change the dispatch queue.
+     By default, the dispatchQueue is 'DispatchQueue.main'
+     */
+    open var receiveDispatchQueue: DispatchQueue {
+        return DispatchQueue.main
+    }
+    
+    /**
+     Thread where the `handle(_:) -> CiMAObservable<Output>` function will be executed.
      This variable can be overridden to change the dispatch queue.
      By default, the dispatchQueue is 'DispatchQueue.global(qos: .background)'
      */
-    open var dispatchQueue: DispatchQueue {
+    open var subscribeDispatchQueue: DispatchQueue {
         return DispatchQueue.global(qos: .background)
     }
     
     // MARK: - Functions
     
+    public init() {}
+    
     /**
-     Main method of executing the interactor.
-     This method executes the `handle(_:_:_:_:)` method on the thread that has been indicated in the `dispatchQueue` variable.
+     Main method of executing the useCase.
+     This method executes the `handle(_:) -> CiMAObservable<Output>` method on the thread that has been indicated in the `subscribeDispatchQueue` variable.
      
      - Parameters:
-     - parameter: The only parameter where it will have all the necessary properties to execute the interactor.
-     - onSuccess: Called when the interactor has been successfully completed.
-     - onFailure: Called in when the interactor has mistakenly ended.
-     - onProcess: It is only called if the `handle(_:_:_:_:)` method is used, passing it to some process to know the progress. For example in an image upload.
-     - onStopped: Called when the interactor has stopped calling the `dispose(_:)`, or has been released from memory.
+     - input: The only parameter where it will have all the necessary properties to execute the useCase.
      
-     - Returns: Disposable object in case the programmer wants to release the interactor before the end of the use case.
+     - Returns: It returns an object of type `CiMAObservable<Output>`.
      */
-    public func execute(_ parameter: Input? = nil,
-                        onSuccess: ((Output) -> Void)? = nil,
-                        onFailure: ((Error) -> Void)? = nil,
-                        onProcess: ((Process) -> Void)? = nil) {
-        dispatchQueue.async { [weak self] in
-            guard let strongSelf = self else {
-                return
-            }
-            
-            if let parameter = parameter, Input.self != Void.self {
-                strongSelf.handle(parameter: parameter,
-                                  onSuccess: onSuccess ?? { _ in },
-                                  onFailure: onFailure ?? { _ in },
-                                  onProcess: onProcess ?? { _ in })
-            } else {
-                strongSelf.handle(onSuccess: onSuccess ?? { _ in },
-                                  onFailure: onFailure ?? { _ in },
-                                  onProcess: onProcess ?? { _ in })
-            }
+    public func execute(_ input: Input? = nil) -> CiMAObservable<Output> {
+        guard let handle = handle(input: input) else {
+            return Fail(error: NSError(domain: "CiMAUseCase handle function failed", code: -10001, userInfo: nil)).eraseToAnyPublisher()
         }
-    }
-    
-    private func getVoidCompletion<T>(_ onCompletion: @escaping (T) -> Void) -> (() -> Void)? {
-        if let onVoidCompletion = onCompletion as? ((()) -> Void) {
-            return { onVoidCompletion(()) }
-        }
-        return nil
+        
+        return handle
+            .subscribe(on: subscribeDispatchQueue)
+            .receive(on: receiveDispatchQueue)
+            .asObservable()
+            .eraseToAnyPublisher()
     }
     
     /**
-     This function must be overridden to add code that we want to execute the use case in the thread indicated in the `dispatchQueue` variable.
-     Use this function when the Interactor has an Input and an Output other than 'Void'.
+     Main method of executing the useCase.
+     This method executes the `handle(input: Input? = nil) async throws -> Output?` which use async/await.
      
      - Parameters:
-     - parameter: Input parameter entered in the `execute(_:_:_:_:_:)` method.
-     - onSuccess: Success completion handler with Output object.
-     - onFailure: Failure completion handler with Error object.
-     - onProcess: Process completion handler with Process object.
+     - input: The only parameter where it will have all the necessary properties to execute the useCase.
+     
+     - Returns: It returns an object of type `Output`.
      */
-    open func handle(parameter: Input,
-                     onSuccess: @escaping (Output) -> Void,
-                     onFailure: @escaping (Error) -> Void,
-                     onProcess: @escaping (Process) -> Void) {
-        if let onSuccessCompletion = getVoidCompletion(onSuccess) {
-            handle(parameter: parameter, onSuccess: onSuccessCompletion, onFailure: onFailure, onProcess: onProcess)
+    public func execute(_ input: Input? = nil) async throws -> Output? {
+        guard let handle = try await handle(input: input) else {
+            return nil
         }
+        return handle
     }
     
     /**
-     This function must be overridden to add code that we want to execute the use case in the thread indicated in the `dispatchQueue` variable.
-     Use this function when the Interactor has Output object only other than 'Void'.
+     This function must be overridden to add code that we want to execute the use case in the thread indicated in the `subscribeDispatchQueue` variable.
      
      - Parameters:
-     - onSuccess: Success completion handler with Output object.
-     - onFailure: Failure completion handler with Error object.
-     - onProcess: Process completion handler with Process object.
-     */
-    open func handle(onSuccess: @escaping (Output) -> Void,
-                     onFailure: @escaping (Error) -> Void,
-                     onProcess: @escaping (Process) -> Void) {
-        if let onSuccessCompletion = getVoidCompletion(onSuccess) {
-            handle(onSuccess: onSuccessCompletion, onFailure: onFailure, onProcess: onProcess)
-        }
-    }
-    
-    /**
-     This function must be overridden to add code that we want to execute the use case in the thread indicated in the `dispatchQueue` variable.
-     Use this function when the Interactor has an Input other than 'Void', but the Output is 'Void' type.
+     - input: The only parameter where it will have all the necessary properties to execute the useCase.
      
-     - Parameters:
-     - parameter: Success completion handler with Output object.
-     - onSuccess: Success completion handler without object.
-     - onFailure: Failure completion handler with Error object.
-     - onProcess: Process completion handler with Process object.
+     - Returns: It returns an object of type `CiMAObservable<Output>`.
      */
-    open func handle(parameter: Input,
-                     onSuccess: @escaping () -> Void,
-                     onFailure: @escaping (Error) -> Void,
-                     onProcess: @escaping (Process) -> Void) {}
-    
-    /**
-     This function must be overridden to add code that we want to execute the use case in the thread indicated in the `dispatchQueue` variable.
-     Use this function when the Interactor has neither Input nor Output. That is, both are 'Void' type.
-     
-     - Parameters:
-     - onSuccess: Success completion handler without object.
-     - onFailure: Failure completion handler with Error object.
-     - onProcess: Process completion handler with Process object.
-     */
-    open func handle(onSuccess: @escaping () -> Void,
-                     onFailure: @escaping (Error) -> Void,
-                     onProcess: @escaping (Process) -> Void) {}
-}
+    open func handle(input: Input? = nil) -> CiMAObservable<Output>? { nil }
 
+    /**
+     This function must be overridden to add code that we want to execute the use case with async/await.
+     
+     - Parameters:
+     - input: The only parameter where it will have all the necessary properties to execute the useCase.
+     
+     - Returns: It returns an object of type `Output`.
+     */
+    open func handle(input: Input? = nil) async throws -> Output? { nil }
+}
